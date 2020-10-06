bba40b315eeac953e13517527e9d6528bdccd428
Andrew Q <C15339351@mytudublin.ie>
2020-10-06 22:16:16 +0100

    Adding files
    Signed-off-by: Andrew Q <C15339351@mytudublin.ie>

---
+++ src/SubscriberDAO.java
@@ -0,0 +1,28 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class SubscriberDAO {
+	
+	//1. Get the EntityManagerFactory 
+	//2. Get the EntityManager from the Factory when we need to talk to the DB
+	//3. Call one of the operations on the EntityManager i.e., persist
+	
+	private static EntityManagerFactory emf = 
+			Persistence.createEntityManagerFactory("AndrewsPU"); 	
+	
+	public SubscriberDAO() {
+	}
+	
+	public void persistSubscriber(Subscriber subscriber) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(subscriber);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	
+
+}
+
